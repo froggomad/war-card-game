309877e8ddcf35568a2681ecdd46eda305ef4365
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-15 09:32:03 -0700

    folder organization/adding UX

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -13,19 +13,20 @@ enum CardChoice: Int {
     case card2
 }
 
-func randomCard() -> (image: String, rank: Int) {
-    let randomNumber = Int.random(in: String.CardRankToNum.allCases[0].rawValue...String.CardRankToNum.allCases.count)
-
-    let card = String.CardRankToNum(rawValue: randomNumber) ?? String.CardRankToNum.two
-
-    return (String.cardImage(card), randomNumber)
-}
-
 struct ContentView: View {
     @State private var randCard1: String = "card2"
     @State private var randCard2: String = "card2"
     @State private var playerScore: Int = 0
     @State private var cpuScore: Int = 0
+    @State private var showCards = false
+
+    func randomCard() -> (image: String, rank: Int) {
+        let randomNumber = Int.random(in: String.CardRankToNum.allCases[0].rawValue...String.CardRankToNum.allCases.count)
+
+        let card = String.CardRankToNum(rawValue: randomNumber) ?? String.CardRankToNum.two
+
+        return (String.cardImage(card), randomNumber)
+    }
 
 
     var body: some View {
@@ -43,28 +44,51 @@ struct ContentView: View {
                 Spacer()
                 // card stack
                 HStack {
-                    
-                    Image(randCard1)
-                    Image(randCard2)
+                    if !showCards {
+                        Image(.image(.back))
+                            .scaleEffect(1)
+                            .animation(.easeInOut)
+                        Image(.image(.back))
+                            .scaleEffect(1)
+                            .animation(.easeInOut)
+                    } else {
+                        Image(randCard1)
+                            .scaleEffect(1.75)
+                            .animation(.easeInOut)
+                        Image(randCard2)
+                            .scaleEffect(1.75)
+                            .animation(.easeInOut)
+                    }
+
                 }
                 // button
                 Spacer()
 
 
                 Button(action: {
-                    let playerCard = randomCard()
-                    print("player rank: \(playerCard.rank)")
-                    let cpuCard = randomCard()
-                    print("CPU rank: \(cpuCard.rank)")
-                    self.randCard1 = playerCard.image
-                    self.randCard2 = cpuCard.image
 
-                    if playerCard.rank > cpuCard.rank {
-                        self.playerScore += 1
-                    } else if cpuCard.rank == playerCard.rank {
-                        print("war!")
-                    } else {
-                        self.cpuScore += 1
+                    withAnimation(.easeInOut(duration: 2)) {
+                        self.showCards.toggle()
+
+                        if self.showCards {
+                            let playerCard = self.randomCard()
+                            print("player rank: \(playerCard.rank)")
+
+                            let cpuCard = self.randomCard()
+                            print("CPU rank: \(cpuCard.rank)")
+                            
+                            self.randCard1 = playerCard.image
+                            self.randCard2 = cpuCard.image
+
+                            //Scoring
+                            if playerCard.rank > cpuCard.rank {
+                                self.playerScore += 1
+                            } else if cpuCard.rank == playerCard.rank {
+                                print("war!")
+                            } else {
+                                self.cpuScore += 1
+                            }
+                        }
                     }
 
                 }) {
